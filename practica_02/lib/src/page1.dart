b5dd87763885d92d829bdfbaf22bca98c3890370
Brini2712 <britsanchez2727@gmail.com>
2025-09-22 17:39:51 -0700

    primer

---
+++ practica_02/lib/src/page1.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.tealAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text('Practica 02'),
+        centerTitle: true,
+      ), // AppBar
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.amber,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Page2');
+              },
+              child: Text('Pagina 2'),
+            ), // ElevatedButton
+            SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Page3');
+              },
+              child: Text('Pagina 3'),
+            ), // ElevatedButton
+          ], // <Widget>[]
+        ), // Column
+      ), // Center
+    ); // Scaffold
+  }
+}
---
+++ practica_02/lib/src/page2.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(title: Text('Pagina 2'), centerTitle: true), // AppBar
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text('Atrás'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ), // ElevatedButton
+          ], // <Widget>[]
+        ), // Column
+      ), // Center
+    ); // Scaffold
+  }
+}
