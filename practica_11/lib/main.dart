0436703ce4bf7827996ba176d1a923e0b3f964f9
Brini2712 <britsanchez2727@gmail.com>
2025-09-29 18:34:42 -0700

    p11

---
+++ practica_11/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+// NO se te olvide importar:
+import 'src/shared_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Practica 11', home: SharedPage());
+  }
+}
