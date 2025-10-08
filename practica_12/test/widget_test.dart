9a4b17d7ae19ef6ba534e6b7bf9df1f6f64fc1fc
Brini2712 <britsanchez2727@gmail.com>
2025-10-08 16:49:25 -0700

    json

--- practica_12/test/widget_test.dart
+++ practica_12/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:practica_10/main.dart';
+import 'package:practica_12/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
